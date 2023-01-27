c582d1a73f8bd1e3d984a384af8acd610316d8aa
shilpa123 <shilpabiradar100@gmail.com>
2023-01-27 11:44:26 +0530

    this is commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+public class Main {
+    public static void main(String[] args) {
+
+
+                int isPresent = 0;
+                int isAbsent = 1;
+
+                int checkAttendance =(int)((Math.E*10)%2);
+                if (checkAttendance==isPresent) {
+                    System.out.println("employee is Present");
+                }
+                else{
+                    System.out.println("employee is Absent");
+                }
+
+            }
+        }
