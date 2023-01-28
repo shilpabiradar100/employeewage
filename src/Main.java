f9849bec308b113f57f992b1afc48516e5457e5c
shilpa123 <shilpabiradar100@gmail.com>
2023-01-28 09:41:24 +0530

    UC2/dailywages

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,20 @@ public class Main {
 
                 int isPresent = 0;
                 int isAbsent = 1;
+                int  dailysalary;
+                int  workHours;
+                int salaryPerhours=20;
 
-                int checkAttendance =(int)((Math.E*10)%2);
+                int checkAttendance =(int)((Math.random()*10)%2);
                 if (checkAttendance==isPresent) {
                     System.out.println("employee is Present");
+                    workHours=8;
                 }
                 else{
                     System.out.println("employee is Absent");
+                    workHours=0;
                 }
-
+                 dailysalary = workHours*salaryPerhours;
+                 System.out.println("daily Salary is " + dailysalary);
             }
         }
